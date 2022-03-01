87db2a4f0d1cdd1948b5858ecb4234067638f682
Ben Bryant <brbryant2639@gmail.com>
2022-03-01 16:30:04 -0600

    UPDATE: added react-ace-editor and socket.io instance

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,67 @@
-import { useState } from "react";
 import Layout from "../components/layout";
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+import ReactAce from "react-ace-editor";
+
+let socket;
+let ace;
 
 export default function Page() {
-  const [term, setTerm] = useState(null);
+  const [isConnected, setIsConnected] = useState(false);
+  const [code, setCode] = useState("console.log('Hello World')");
 
-  const loadTerm = async () => {
-    let res = await fetch("/api/terms/single");
-    let data = await res.json();
-    setTerm(data);
+  const onChange = (updatedCode) => {
+    setCode(updatedCode);
+    socket.emit("update-code", updatedCode);
   };
+
+  const initializeSocket = async () => {
+    await fetch("/api/socket");
+    socket = io();
+
+    socket.on("connect", () => {
+      console.log("STATUS: connected");
+      setIsConnected(true);
+    });
+
+    socket.on("update-code", (value) => setCode(value));
+  };
+
+  useEffect(() => {
+    initializeSocket();
+  }, []);
 
   return (
     <Layout>
-      <h1>BB Terms (NextAuth.js)</h1>
+      <h1>BB Terms (NextAuth.js x Socket.io)</h1>
       <p>
         This is an example site to demonstrate how to use{" "}
-        <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
+        <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication
+        and <a href="https://socket.io">Socket.io</a> for real-time
+        communication.
       </p>
-      <button onClick={loadTerm}>Load Term</button>
-      <div>
-        <h2>{term?.term || "TERM"}</h2>
-        <p>{term?.def || "DEFINITION"}</p>
-        <p>
-          Learn more at <a href={term?.src || "#"}>{term?.src || "SOURCE"}</a>
-        </p>
-      </div>
+      <p>
+        Socket Status:{" "}
+        <sup
+          style={{
+            display: "inline-block",
+            backgroundColor: isConnected ? "green" : "red",
+            width: "10px",
+            height: "10px",
+            borderRadius: "9999px",
+          }}
+        ></sup>
+      </p>
+      <ReactAce
+        mode="javascript"
+        theme="eclipse"
+        setReadOnly={false}
+        onChange={onChange}
+        style={{ width: "100%", minHeight: "300px" }}
+        ref={(instance) => {
+          ace = instance;
+        }}
+      />
     </Layout>
   );
 }
